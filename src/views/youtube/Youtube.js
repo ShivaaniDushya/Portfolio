8365511318c90eab0779c07252ca14ffd3daba22
ShivaaniDushya <it20006884@my.sliit.lk>
2022-02-27 23:44:53 +0530

    Youtube page updated

--- src/views/youtube/Youtube.js
+++ src/views/youtube/Youtube.js
@@ -2,21 +2,83 @@ import React from "react";
 
 const Youtube = ()=>{
     return(
-        <div className="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-2 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-3">
-            
-            <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-            <a href='https://youtu.be/_-i8p54OaGM'>
-            <img src="./images/speaking/ITP-GUIDE Shivaani-02.png"/>
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 a">Introduction to REST API and Postman</h2>
-            <h2 className="font-medium tracking-tight text-gray-900 sm:text-1xl b">
-            ITP Guide (Industry Training Project) was a special event , which was organized and conducted by the MS Club of SLIIT for the past two years.
+        
+        <div className="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
+          <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
+      
+          <img
+            src="./images/speaking/abc.png"
+            alt="Shivaani Dushya"
+            className="bg-gray-100 rounded-lg"
+            width={1000}
+            height={1000}
+          />
+        
+        </div>
+
+      <div>
+        
+        <p className="mt-5 text-gray-500 sm:text-2xl">
+        Introduction to REST API and Postman
+        </p>
+        <p className="mt-5 text-gray-500 sm:text-lg">
+             ITP Guide (Industry Training Project) was a special event , which was organized and conducted by the MS Club of SLIIT for the past two years.
             On 16th of January 2022, we conducted the ITP Guide for the third consecutive time. The event mainly focuses on giving an awareness on how to create industry level projects.
             I have done a session on "Introduction to Restful Web Services and Postman".
-            </h2>
-            </a>
-            </div>
+        </p><br/>
+        <a href="https://youtu.be/_-i8p54OaGM?list=PLM1HdceeXpsOqJTw1GVzIOqrLyrg_xDTu">
+        <button class="bg-white hover:bg-gray-100 text-gray-800 font-medium py-2 px-2 border border-gray-400 rounded shadow sm:text-sm">
+          Video Link
+          </button>
+        </a>
+      <br/>
+      <br/>
 
+      <div className="xl:grid-cols-32 xl:gap-x-4">
+
+      </div>
+      </div>
+
+
+      <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
+      
+          <img
+            src="./images/speaking/session2itp.jpg"
+            alt="Shivaani Dushya"
+            className="bg-gray-100 rounded-lg"
+            width={1000}
+            height={1000}
+          />
+        
         </div>
+
+      <div>
+        
+        <p className="mt-5 text-gray-500 sm:text-2xl">
+        Backend with Spring Boot
+        </p>
+        <p className="mt-5 text-gray-500 sm:text-lg">
+        In ITP Guide 3.0 - session 2, we have created a Todo application using trending technologies and Frameworks.
+        Me and Shehan done the session on "Backend with Spring Boot".
+        </p><br/>
+        <a href="https://youtu.be/-LaIxxxIEmY">
+        <button class="bg-white hover:bg-gray-100 text-gray-800 font-medium py-2 px-2 border border-gray-400 rounded shadow sm:text-sm">
+          Video Link
+          </button>
+        </a>
+      <br/>
+      <br/>
+
+      <div className="xl:grid-cols-32 xl:gap-x-4">
+
+      </div>
+      </div>
+      </div>
+
+        
+
+
+
     );
 }
 
