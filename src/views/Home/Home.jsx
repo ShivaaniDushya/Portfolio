02932c0eceeae0afb6a2bbac7cbebfa77d9c9484
ShivaaniDushya <it20006884@my.sliit.lk>
2022-07-24 19:17:36 +0530

    technologies bg colour updated

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -474,7 +474,7 @@ const Home = () => {
         <div className="grid grid-cols-4 gap-y-10 sm:grid-cols-3 gap-x-32 lg:grid-cols-5 xl:grid-cols-6 xl:gap-x-32 ">
           {products.map((product) => (
             <a key={product.id} className="group">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <div className="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={product.imageSrc}
                   alt={product.imageAlt}
@@ -507,7 +507,7 @@ const Home = () => {
         <div className="grid grid-cols-4 gap-y-10 sm:grid-cols-3 gap-x-32 lg:grid-cols-5 xl:grid-cols-6 xl:gap-x-32">
           {tools.map((tool) => (
             <a key={tool.id} className="group">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 ">
+              <div className="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 ">
                 <img
                   src={tool.imageSrc}
                   alt={tool.imageAlt}
